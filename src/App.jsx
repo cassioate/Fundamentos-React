355e33eee26235ff45d41002127da7151b267bd1
Cassio Tessaro <cassioate@gmail.com>
2020-10-15 05:34:59 -0300

    Mega

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import DiretaPai from './Componentes/comunicacao/DiretaPai'
 import IndiretaPai from './Componentes/comunicacao/IndiretaPai'
 import Input from './Componentes/formulario/input'
 import Contador from './Componentes/contador/Contador'
+import Mega from './Componentes/mega/Mega'
 
 
 export default () =>
@@ -26,26 +27,31 @@ export default () =>
 
         <div className="Cards">
 
-            <Card titulo="#13 - Contador" color="#E6E6FA">
+            <Card titulo="#13 - Mega" color="#E6E6FA">
+                <Mega>
+                </Mega>
+            </Card>
+
+            <Card titulo="#12 - Contador" color="#E6E6FA">
                 <Contador>
                 </Contador>
             </Card>
 
-            <Card titulo="#12 - Componente Controlado (Input)" color="#8BAD39">
+            <Card titulo="#11 - Componente Controlado (Input)" color="#8BAD39">
                 <Input>
                 </Input>
             </Card>
 
-            <Card titulo="#11 - Comunicação Indireta" color="#5555fc">
+            <Card titulo="#10 - Comunicação Indireta" color="#5555fc">
                 <IndiretaPai>
                 </IndiretaPai>
             </Card>
 
-            <Card titulo="#10 - Comunicação Direta" color="#6495ed">
+            <Card titulo="#9 - Comunicação Direta" color="#6495ed">
                 <DiretaPai></DiretaPai>
             </Card>
 
-            <Card titulo="#09 - Renderização Condicional" color="#FF6C55">
+            <Card titulo="#08 - Renderização Condicional" color="#FF6C55">
                 <ParOuImpar
                     numero={20}
                 />
@@ -54,24 +60,20 @@ export default () =>
                 />
             </Card>
 
-            <Card titulo="#08 - Tabela de Produtos" color="#FF6C55">
+            <Card titulo="#07 - Tabela de Produtos" color="#FF6C55">
                 <TabelaProdutos />
             </Card>
 
-            <Card titulo="#07 - Lista de Alunos" color="#FF4C65">
+            <Card titulo="#06 - Lista de Alunos" color="#FF4C65">
                 <ListaAlunos />
             </Card>
 
-            <Card titulo="#06 - Componente com filhos" color="#00C8F8">
+            <Card titulo="#05 - Componente com filhos" color="#00C8F8">
                 <Familia sobrenome="Ferreira">
                     <FamiliaMembro nome="Pedro" />
                     <FamiliaMembro nome="Ana" />
                     <FamiliaMembro nome="Gustavo" />
                 </Familia>
-            </Card>
-
-            <Card titulo="#05 - Primeiro" color="#080">
-                <Primeiro></Primeiro>
             </Card>
 
             <Card titulo="#04 - Desafio aleatorio" color="#FFFACD">
@@ -81,7 +83,12 @@ export default () =>
                 />
             </Card>
 
-            <Card titulo="#03 - Com parametro" color="#8B8989">
+            <Card titulo="#03 - Fragmento" color="#E0EEE0">
+                <Fragmento
+                />
+            </Card>
+
+            <Card titulo="#02 - Com parametro" color="#8B8989">
                 <ComParametro
                     titulo="Segundo Componente"
                     aluno="Cassio"
@@ -97,12 +104,8 @@ export default () =>
                 />
             </Card>
 
-            <Card titulo="#01 - Fragmento" color="#E0EEE0">
-                <Fragmento
-                    titulo="Segundo Componente"
-                    aluno="José"
-                    nota={5}
-                />
+            <Card titulo="#01 - Primeiro" color="#080">
+                <Primeiro></Primeiro>
             </Card>
         </div>
     </div>
